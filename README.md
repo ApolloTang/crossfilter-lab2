b484d3db33b054ec0667f8eed70c0047012cc24a
Apollo Tang <tang.apollo@gmail.com>
2018-03-17 16:07:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+## Crossfilter lab
+
+
+```
+$ npm install -g browser-sync
+$ npm install -g http-server
+```
+
+Then
+
+```
+$ ./start.sh
+```
+
+
---
+++ resources/print-filter.js
@@ -0,0 +1,18 @@
+function print_filter(filter) {
+    var f=eval(filter);
+    if (typeof(f.length) != "undefined") {}else{}
+    if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
+    if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
+    console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
+}
+
+function pObj (o) {
+  console.log(
+    JSON.stringify(o)
+    .replace("[","[\n\t")
+    .replace(/}\,/g,"},\n\t")
+    .replace("]","\n]"));
+  return'pObj done'
+}
+
+const l = console.log
